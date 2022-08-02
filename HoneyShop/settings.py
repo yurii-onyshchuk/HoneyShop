75f7e005760c866dc85969d5de9c5afbdf37cfcd
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2022-08-03 00:43:21 +0300

    Add first URL path and empty files

--- HoneyShop/settings.py
+++ HoneyShop/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure--&*9fd!@f)abt#f(=s+ot+)abb7@+9#k@lnswe8mf=q^hv-c@k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 # Application definition
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'HoneyShop', 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
