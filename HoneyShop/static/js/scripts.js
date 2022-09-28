dde73dcadd629ea31fee498c28f0c51f49401457
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2022-09-29 00:59:05 +0300

    Prepared about-us page

--- HoneyShop/static/js/scripts.js
+++ HoneyShop/static/js/scripts.js
@@ -23,4 +23,15 @@ $(document).ready(function () {
             $(this).addClass('active');
         }
     })
-})
+});
+
+document.getElementById('lightbox-links').onclick = function (event) {
+    event = event || window.event
+    const target = event.target || event.srcElement;
+    const link = target.src ? target.parentNode : target;
+    const options = {index: link, event: event};
+    const links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options)
+    const blueimp_gallery = document.getElementById('blueimp-gallery')
+    blueimp_gallery.classList.add('blueimp-gallery-controls')
+}
