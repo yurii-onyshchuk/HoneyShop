857a5f1a39c1ca23f93af3d41fb3ab53315e366e
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2022-10-17 15:27:00 +0300

    Added models for shop: Category, Product, Shop

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import Category, Product, Comment
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    save_on_top = True
+    list_display = ('id', 'title', 'price', 'category', 'available', 'created_at', 'get_photo')
+    list_display_links = ('id', 'title',)
+    list_filter = ('category', 'available',)
+    search_fields = ('title',)
+    fields = ('title', 'slug', 'price', 'available', 'category', 'description', 'photo', 'get_photo', 'sales', 'views', 'created_at',)
+    readonly_fields = ('created_at', 'views', 'sales', 'get_photo',)
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<a href="{obj.photo.url}"><img src="{obj.photo.url}" width="80"></a>')
+        return '-'
+
+    get_photo.short_description = 'Мініатюра'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'body', 'created_at', 'user')
+    list_display_links = ('id',)
+    list_filter = ('product', 'user')
+    search_fields = ('body',)
+    fields = ('product', 'body', 'created_at', 'user',)
+    readonly_fields = ('product', 'body', 'created_at', 'user',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Назва категорії товару')
+    slug = models.SlugField(max_length=100, verbose_name='URL', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Категорія товару'
+        verbose_name_plural = 'Категорії товарів'
+        ordering = ['title']
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products', verbose_name='Категорія')
+    title = models.CharField(max_length=250, verbose_name='Назва')
+    slug = models.SlugField(max_length=250, verbose_name='URL', unique=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Фото')
+    description = models.TextField(blank=True, verbose_name='Опис')
+    price = models.DecimalField(verbose_name='Ціна', max_digits=7, decimal_places=2)
+    available = models.DecimalField(default=0, verbose_name='Доступно', max_digits=7, decimal_places=2)
+    sales = models.IntegerField(default=0, verbose_name='Продажі')
+    views = models.IntegerField(default=0, verbose_name='Перегляди')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата додавання')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product', kwargs={'slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товари'
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', verbose_name='Товар')
+    user = models.ForeignKey(User, on_delete=models.SET('Видалений користувач'), related_name='comments',
+                             verbose_name='Користувач')
+    body = models.TextField(verbose_name='Коментар')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата додавання')
+
+    def __str__(self):
+        return f'Коментар на {self.product} від {self.user}'
+
+    class Meta:
+        verbose_name = 'Коментар'
+        verbose_name_plural = 'Коментарі'
+        ordering = ['-created_at']
